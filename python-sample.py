0bd74c99401a4385d3830d1db4f7ef238597c9fc
Eli Bosley <elibosley@users.noreply.github.com>
2017-07-07 13:38:54 -0400

    Update python-sample.py

--- python-sample.py
+++ python-sample.py
@@ -1,13 +1,23 @@
+
 def wombat(state, time_left):
     # Note that the function name MUST be wombat
+    import random
+    theAction = ""
+    availableActions = ["turn", "shoot", "move"]
+    availableMetadata = {"turn": {"direction" : 'right'}, "shoot": {}, "move": {}}
+    
+    index = random.randint(1,2)
+    
+    action = availableActions[index]
+    metadata = availableMetadata[action]
+    
+    if state['global-coords'][1] == state['global-dimensions'][1]:
+        action = availableActions[0]
+        metadata = availableMetadata[action]
     return {
         'command': {
-            'action': 'turn',
-            'metadata': {
-                'direction': 'right'
-            }
+            'action': action,
+            'metadata': metadata
         },
-        'state': {
-            'hello': 'world'
-        }
+        'state': state
     }
