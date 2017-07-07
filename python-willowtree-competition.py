23fb941e1cf69ccc8d9544a8c18434a9fa0a3d7f
Eli Bosley <elibosley@users.noreply.github.com>
2017-07-07 13:17:03 -0400

    Update python-willowtree-competition.py

--- python-willowtree-competition.py
+++ python-willowtree-competition.py
@@ -1,1 +1,13 @@
-
+def wombat(state, time_left):
+    # Note that the function name MUST be wombat
+    return {
+        'command': {
+            'action': 'turn',
+            'metadata': {
+                'direction': 'right'
+            }
+        },
+        'state': {
+            'hello': 'world'
+        }
+    }
