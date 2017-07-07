a12d7774b6ff2700faa4388b3b24a244a9d332a0
Eli Bosley <elibosley@users.noreply.github.com>
2017-07-07 15:42:06 -0400

    Update python-willowtree-competition.py

--- python-willowtree-competition.py
+++ python-willowtree-competition.py
@@ -1,24 +1,56 @@
 
+
 def wombat(state, time_left):
     # Note that the function name MUST be wombat
+    turnRightAction = {'action': 'turn', 'metadata' : {'direction': 'right'}}
+    turnLeftAction = {'action': 'turn', 'metadata' : {'direction': 'left'}}
+    moveAction = {'action': 'move', 'metadata': {}}
+    shootAction = {'action': 'shoot', 'metadata': {}}
+
+
+    def updateFacingDirection (currentDirection, action):
+    	if action['action'] == 'turn':
+    		if action['metadata']['direction'] == 'left':
+    			if currentDirection == 'up':
+    				return 'left'
+    			if currentDirection == 'left':
+    				return 'down'
+    			if currentDirection == 'down':
+    				return 'right'
+    			if currentDirection == 'right':
+    				return 'up'
+    		if action['metadata']['direction'] == 'right':
+    			if currentDirection == 'up':
+    				return 'right'
+    			if currentDirection == 'left':
+    				return 'up'
+    			if currentDirection == 'down':
+    				return 'left'
+    			if currentDirection == 'right':
+    				return 'down'
+	else:
+		return currentDirection
+		
+    
     import random
     theAction = ""
-    availableActions = ["turn", "shoot", "move"]
-    availableMetadata = {"turn": {"direction" : 'right'}, "shoot": {}, "move": {}}
+    availableActions = [turnRightAction, turnLeftAction, moveAction, shootAction]
     
-    index = random.randint(1,2)
+    if 'direction' not in state:
+        state['direction'] = "right"
     
+    index = random.randint(2,3)
     action = availableActions[index]
-    metadata = availableMetadata[action]
     
-    if state['global-coords'][0] == (state['global-dimensions'][0] - 1) and state != 'turned-right':
+    if state['global-coords'][0] == (state['global-dimensions'][0] - 1) and state['direction'] == "right":
         action = availableActions[0]
-        metadata = availableMetadata[action]
-        state = 'turned-right'
+        
+    state['direction'] = updateFacingDirection(state['direction'], turnRightAction)
     return {
         'command': {
-            'action': action,
-            'metadata': metadata
+            'action': action['action'],
+            'metadata': action['metadata']
         },
         'state': state
     }
+
